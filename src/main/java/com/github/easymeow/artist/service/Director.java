4740c98a2cc5a6e7bc0206946f67f139453a9540
Kirill Shashov <envoy93@gmail.com>
2021-04-09 15:39:52 +0400

    Impoved javadocs

--- src/main/java/com/github/easymeow/artist/service/Director.java
+++ src/main/java/com/github/easymeow/artist/service/Director.java
@@ -19,6 +19,8 @@ public class Director implements Producer {
 
     /**
      * создание альбома
+     *
+     * @param album album to release
      */
     @Override
     public void createRelease(Musician artist, Release album) {
